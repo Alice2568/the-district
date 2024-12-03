935c64ec9af1a6088928f1be0111462fe8320170
Marissal Alice <alice.ternois@orange.fr>
2024-12-03 16:33:41 +0100

    majphp

--- accueil.php
+++ accueil.php
@@ -95,14 +95,7 @@
     </div>
     
 
-      <footer>
-        <div >
-        <img class="image-reseau" src="img/instagram_2111463.png" alt="logo instagram"><img class="image-reseau" src="img/square-x_10455437.svg" alt="logo X"><img class="image-reseau" src="img/social_12942327.png" alt="logo facebook">
-        </div>
-      </footer>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+      <?php require"footer.php"; ?>
 </body>
 
 </html>
--- categories.php
+++ categories.php
@@ -53,9 +53,7 @@ require("header.php");
   </section>
   <!--liste des catégories + image cliquable. Double image ? défilement des images quand mouse hover?
             titre des catégories = Nos pizzas , Nos burgers etc-->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+            <?php require"footer.php"; ?>
 </body>
 
 </html>
