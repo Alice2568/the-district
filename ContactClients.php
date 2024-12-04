c8d041653199acad7125a71dc490c46e0d95506e
Marissal Alice <alice.ternois@orange.fr>
2024-12-04 16:48:10 +0100

    majcontact

---
+++ ContactClients.php
@@ -0,0 +1,7 @@
+<?php 
+class contact {
+    public function __construct(string $name, string $telephone, ?string $surname = null, ?string $email = null, ?string $demande = null) {
+
+    }
+    public function isValid(): bool {}
+}
--- contact.php
+++ contact.php
@@ -1,23 +1,51 @@
-<?php require 'header.php'; ?>
+<?php
+require_once 'ContactClients.php';
+if (isset($_POST["name"],$_POST["telephone"])) {
+    $contact = new Contact($_POST["name"], $_POST["telephone"]);
+    if ($contact->isValide()){
 
-    <h2> Nous contacter</h2>
-    <form action="contact.php">
-        <fieldset>
-            <legend>Nous contacter</legend>
-            <label for="name">Nom</label>
-            <input type="text" name="name" id="name">
-            <label for="firstname">Prénom</label>
-            <input type="text" name="firstname" id="firstname">
-            <label for="emailinput">Email</label>
-            <input type="text" name="email" id="emailinput">
-            <label for="telephone">Téléphone</label>
-            <input type="text" name="telephone" id="telephone">
-            <label for="question">Votre demande</label>
-            <input type="textarea" name="question" id="question">
-
-        </fieldset>
+    } else {
+        $errors = "Formulaire invalide";
+    }
+}
+require 'header.php'; ?>
+</head>
+<body>
+    <h1>Nous Contacter</h1>
+    <?php if ($errors): ?>
+        <div class="alert alert-danger">
+            <?= $errors ?>
+        </div>
+        <?php endif; ?>
+    <form action="" method="post">
+        <div class="form-group">
+            <div>
+                <label for="nom">Nom</label>
+                <input type="text" id="name" name="name" placeholder="Entrez votre nom" class="form-control" required>
+            </div>
+            <div>
+                <label for="prenom">Prénom</label>
+                <input type="text" id="surname" name="surname" placeholder="Entrez votre prénom" class="form-control">
+            </div>
+        </div>
+        <div class="form-group">
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Entrez votre email" class="form-control">
+            </div>
+            <div>
+                <label for="telephone">Téléphone</label>
+                <input type="tel" id="telephone" name="telephone" placeholder="Entrez votre téléphone" class="form-control" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <div>
+                <label for="demande">Votre demande</label>
+                <textarea id="demande" name="demande" placeholder="Décrivez votre demande ici..." class="form-control"></textarea>
+            </div>
+        </div>
+        <button class="btn btn-danger " type="submit">Envoyer</button>
     </form>
     <?php require"footer.php"; ?>
 </body>
-
 </html>
